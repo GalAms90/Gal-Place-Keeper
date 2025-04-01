a81457fa821426a1c49f1945249d1511d5381add
GalAms90 <Gams2597@gmail.com>
2025-04-01 13:38:10 +0300

    add basic form layout

--- js/userService.js
+++ js/userService.js
@@ -1,2 +1,34 @@
 'use strict'
 
+
+
+function onSubmit(ev) {
+  ev.preventDefault()
+
+
+  let bgColor = document.querySelector('#background-color').value
+  let txtColor = document.querySelector('#text-color').value
+  let bod = document.querySelector('#birth-date').value
+  let bot = document.querySelector('#birth-time').value
+
+  const userData = { bgColor, txtColor, bod, bot }
+  saveToStorage('userData', userData)
+
+  document.body.style.backgroundColor = bgColor
+  document.body.style.color = txtColor
+}
+
+function applyUserSettings() {
+  const userData = loadFromStorage('userData')
+  if (!userData) return
+
+  const { bgColor, txtColor, vod, bot } = userData
+
+  document.body.style.backgroundColor = bgColor
+  document.body.style.color = txtColor
+
+  document.querySelector('#background-color').value = bgColor
+  document.querySelector('#text-color').value = txtColor
+  document.querySelector('#birth-date').value = bod
+  document.querySelector('#birth-time').value = bot
+}
