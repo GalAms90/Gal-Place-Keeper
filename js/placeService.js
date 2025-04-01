0529acff5c45d677b78a3529c2b52e7819ca88fa
GalAms90 <Gams2597@gmail.com>
2025-04-01 17:37:40 +0300

    add google maps API

--- js/placeService.js
+++ js/placeService.js
@@ -1,1 +1,22 @@
 'use strict'
+
+function initMap(lat, lng) {
+  if (!lat) lat = 32.0749831
+  if (!lng) lng = 34.9120554
+  var elMap = document.querySelector('.map')
+  var options = {
+    center: { lat, lng },
+    zoom: 8
+  }
+
+  var map = new google.maps.Map(
+    elMap,
+    options
+  )
+
+  var marker = new google.maps.Marker({
+    position: { lat, lng },
+    map,
+    title: 'Hello World!'
+  })
+}
